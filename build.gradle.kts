6e68d91cf5794c5317c666127f89731e4f0e7d32
eike <98343869+EikeMB@users.noreply.github.com>
2023-10-06 13:09:19 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,27 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    application
+    kotlin("jvm")
+    id("org.jlleitschuh.gradle.ktlint")
+}
+
+application {
+    mainClass.set("org.jlleitschuh.gradle.ktlint.sample.kotlin.MainKt")
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+    filter {
+        exclude("**/style-violations.kt")
+    }
 }
