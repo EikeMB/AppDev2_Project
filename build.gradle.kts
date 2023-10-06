4c826bb7ed33921d1c9a416f49b49060ff63ca52
eike <98343869+EikeMB@users.noreply.github.com>
2023-10-06 16:16:49 -0400

    Update build.gradle.kts (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "17" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     application
     kotlin("jvm") version "1.3.10"
