263e545796d0a252d7a56c53ec8d5e78a9735dc2
eike <98343869+EikeMB@users.noreply.github.com>
2023-10-06 16:40:46 -0400

    Update build.gradle.kts (#18)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "0.34.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6"
 }
 
 
