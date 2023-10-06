308b09838b1d4f20ef15c50a460d5ee8a6e8e9aa
eike <98343869+EikeMB@users.noreply.github.com>
2023-10-06 16:26:23 -0400

    Update build.gradle.kts (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,17 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id "org.jlleitschuh.gradle.ktlint" version "7.1.0"
 }
 
+
+buildscript {
+  repositories {
+    maven {
+      url "https://plugins.gradle.org/m2/"
+    }
+  }
+  dependencies {
+    classpath "org.jlleitschuh.gradle:ktlint-gradle:7.1.0"
+  }
+}
