802d25214379feec5bb58e1044e604c2b4834982
eike <98343869+EikeMB@users.noreply.github.com>
2023-10-30 16:24:02 -0400

    added all classes

--- app/src/main/java/com/example/appdev2_assignment2/CarPart.kt
+++ app/src/main/java/com/example/appdev2_assignment2/CarPart.kt
@@ -1,6 +1,6 @@
 package com.example.appdev2_assignment2
 
-class CarPart(var modelNum: Int, var description: String) {
+open class CarPart(var modelNum: Int, var description: String) {
 
 
     lateinit var combatibilityList: List<CarPart>
--- app/src/main/java/com/example/appdev2_assignment2/Parts.kt
+++ app/src/main/java/com/example/appdev2_assignment2/Parts.kt
@@ -1,4 +1,31 @@
 package com.example.appdev2_assignment2
 
-class Parts {
+class Body(var name:String, var image:Int, modelNum: Int, description: String):CarPart(modelNum, description) {
+
+
+}
+
+class Engine(var name:String, var image:Int, modelNum: Int, description: String):CarPart(modelNum, description) {
+
+
+}
+
+class Wheels(var name:String, var image:Int, modelNum: Int, description: String):CarPart(modelNum, description) {
+
+
+}
+
+class Aerodynamics(var name:String, var image:Int, modelNum: Int, description: String):CarPart(modelNum, description) {
+
+
+}
+
+class Interior(var name:String, var image:Int, modelNum: Int, description: String):CarPart(modelNum, description) {
+
+
+}
+
+class Accessories(var name:String, var image:Int, modelNum: Int, description: String):CarPart(modelNum, description) {
+
+
 }
