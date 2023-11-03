402b7e7e4b6b4cb8fe9e059ff2aac403a34e0e7b
JiahaoYu1 <98350594+JiahaoYu1@users.noreply.github.com>
2023-11-03 14:30:49 -0400

    data added (#40)

--- app/src/main/java/com/example/appdev2_assignment2/Car.kt
+++ app/src/main/java/com/example/appdev2_assignment2/Car.kt
@@ -7,7 +7,14 @@ import androidx.compose.ui.res.painterResource
 
 
 class Car(var name:String, var parts: List<CarPart>, var vin:Int) {
+
 }
+var cars: List<Car> = listOf(
+    Car("nissan skyline r34", parts1,123),
+    Car("Ferrari F430", parts2, 456),
+    Car("Porsche 911 GT3", parts3, 789)
+)
+
 
 
 
