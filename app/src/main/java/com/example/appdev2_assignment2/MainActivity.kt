d03652c462c16b8ed4390dd922fac73fe648e58a
Yensan2B <90729827+Yensan2B@users.noreply.github.com>
2023-11-05 14:36:05 -0500

    Login/sign up UI (#42)
    * Created, login page Ui
    
    Created, login page Ui
    
    * Sign up Ui done
    
    * adujust size of sign up page
    
    * add validation
    
    * Setup navigation
    
    * Update MainActivity.kt
    
    -output error
    -change signup placeholder to label
    -add nice comment for readability

--- app/src/main/java/com/example/appdev2_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/appdev2_assignment2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appdev2_assignment2
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.NumberPicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -41,6 +42,7 @@ import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -58,6 +60,8 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,6 +71,10 @@ import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.navigation.NavController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,7 +86,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyApp()
+                    StartupPage()
 
                 }
             }
@@ -87,13 +95,33 @@ class MainActivity : ComponentActivity() {
 
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StartupPage() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "LoginScreenRoute") {
+        composable("LoginScreenRoute") {
+            LoginScreen(navController)
+        }
+        composable("SignUpScreenRoute") {
+            SignUpScreen(navController)
+        }
+        composable("MainScreenRoute") {
+            MyApp()
+        }
+    }
+}
+
+
 /*
 Composable made up of the full page
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyApp(){
-    
+fun MyApp() {
     val navController = rememberNavController()
 
     Scaffold(
@@ -107,7 +135,7 @@ fun MyApp(){
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues),
-                        color = MaterialTheme.colorScheme.background
+                color = MaterialTheme.colorScheme.background
             ) {
 
                 Router(navController)
@@ -116,16 +144,20 @@ fun MyApp(){
         },
         bottomBar = {
             BottomAppBar {
-                IconButton(onClick = { navController.navigate("MainScreenRoute")} ,modifier = Modifier.weight(1f)) {
+                IconButton(onClick = { navController.navigate("MainScreenRoute") },
+                    modifier = Modifier.weight(1f)) {
                     Icon(Icons.Filled.Home, contentDescription = "Home")
                 }
-                IconButton(onClick = { navController.navigate("AboutScreenRoute") },modifier = Modifier.weight(1f)) {
+                IconButton(
+                    onClick = { navController.navigate("AboutScreenRoute") },
+                    modifier = Modifier.weight(1f)
+                ) {
                     Icon(Icons.Filled.Build, contentDescription = "Create")
                 }
-                IconButton(onClick = {  },modifier = Modifier.weight(1f)) {
+                IconButton(onClick = { }, modifier = Modifier.weight(1f)) {
                     Icon(Icons.Filled.Person, contentDescription = "User")
                 }
-                IconButton(onClick = { /*TODO*/ },modifier = Modifier.weight(1f)) {
+                IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
                     Icon(Icons.Filled.Face, contentDescription = "About Us")
                 }
             }
@@ -133,7 +165,231 @@ fun MyApp(){
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // TITLE
+        Text(
+            text = "App Title", // Your title text
+            modifier = Modifier.padding(top = 50.dp, bottom = 125.dp)
+        )
+
+        //USERNAME INPUT
+        TextField(
+            value = username,
+            onValueChange = { newText ->
+                username = newText
+            },
+            placeholder = { Text("Enter your Username") }, // Add the placeholder
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        //PASSWORD INPUT
+        TextField(
+            value = password,
+            onValueChange = { newText ->
+                password = newText
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            placeholder = { Text("Enter your Password") }, // Add the placeholder
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        //LOGIN BUTTON
+        Button(
+            modifier = Modifier.padding(vertical = 10.dp),
+            onClick = { navController.navigate("MainScreenRoute") },
+        ) {
+            Text("Login")
+        }
+
+        //SIGN UP BUTTON
+        Button(
+            onClick = { navController.navigate("SignUpScreenRoute") },
+        ) {
+            Text("Sign up")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var age by remember { mutableStateOf("18") }
+
+    // ERROR INPUT FIELD
+    var usernameError by remember { mutableStateOf<String?>(null) }
+    var passwordError by remember { mutableStateOf<String?>(null) }
+    var confirmPasswordError by remember { mutableStateOf<String?>(null) }
+    var ageError by remember { mutableStateOf<String?>(null) }
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // TITLE
+        Text(
+            text = "App Title", // Your title text
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+
+        // USERNAME INPUT
+        TextField(
+            value = username,
+            onValueChange = { newText ->
+                username = newText
+            },
+            label = { Text("Enter your Username") }, // Add the placeholder
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            isError = usernameError != null,
+        )
+        usernameError?.let { error ->
+            Text(text = error, color = Color.Red, modifier = Modifier.padding(vertical = 5.dp))
+        }
+
+        //PASSWORD INPUT
+        TextField(
+            value = password,
+            onValueChange = { newText ->
+                password = newText
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            label = { Text("Enter your Password") }, // Add the placeholder
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            isError = passwordError != null,
+        )
+        passwordError?.let { error ->
+            Text(text = error, color = Color.Red, modifier = Modifier.padding(vertical = 5.dp))
+        }
+
+        //CONFIRM PASSWORD
+        TextField(
+            value = confirmPassword,
+            onValueChange = { newText ->
+                confirmPassword = newText
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            label  = { Text("Enter your Confirm Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            isError = confirmPasswordError != null,
+        )
+        confirmPasswordError?.let { error ->
+            Text(text = error, color = Color.Red, modifier = Modifier.padding(8.dp))
+        }
+
+        //AGE BOX
+        TextField(
+            value = age,
+            onValueChange = { newText ->
+                age = newText
+            },
+            label = { Text("Enter your Age") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            isError = ageError != null,
+        )
+        ageError?.let { error ->
+            Text(text = error, color = Color.Red, modifier = Modifier.padding(vertical = 5.dp))
+        }
+
+        //USER'S IMAGE
+        ImageChangingButton();
+
+        //REGISTER BUTTON
+        Button(
+            modifier = Modifier.padding(vertical = 10.dp),
+            onClick = {
+                if (validateInputSignUp(username, password, confirmPassword, age)) {
+                    navController.navigate("LoginScreenRoute")
+                } else {
+                    usernameError = if (username.isEmpty()) "Username required" else null
+                    passwordError = if (password.isEmpty()) "Password  required" else null
+                    confirmPasswordError = if (password != confirmPassword) "Confirm Password does not match Password" else null
+                    ageError = if (age.isEmpty() || age.toIntOrNull() == null) "Invalid age" else null
+                }
+            },
+            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary)
+        ) {
+            Text("Register")
+        }
+
+        //LOGIN BUTTON
+        Button(
+            onClick = {navController.navigate("LoginScreenRoute")},
+            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary)
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+@Composable
+fun ImageChangingButton() {
+    var isImage1Visible by remember { mutableStateOf(true) }
+    val currentImage = if (isImage1Visible) R.drawable.ferrari else R.drawable.mercedes
+
+    Button(
+        onClick = {
+            isImage1Visible = !isImage1Visible
+        }
+    ) {
+        Image(
+            painter = painterResource(id = currentImage),
+            contentDescription = "Image",
+            modifier = Modifier.size(50.dp)
+        )
+    }
+}
+
+fun validateInputSignUp(
+    username: String,
+    password: String,
+    confirmPassword: String,
+    age: String
+): Boolean {
+    val isUsernameValid = username.isNotEmpty()
+    val isPasswordValid = password.isNotEmpty()
+    val doesPasswordsMatch = password == confirmPassword
+    val isAgeValid = age.toIntOrNull() != null
+
+    return isUsernameValid && isPasswordValid && doesPasswordsMatch && isAgeValid
+}
 
 @Composable
 fun Title(){
